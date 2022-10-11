4a9ea27fd84910810c624e60702dcb94ff349e68
Joao Felipe <jfcbxp@gmail.com>
2022-10-11 08:13:19 -0300

    adicionado estilo ao card da receita

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import '../components/category_item.dart';
 import '../data/dummy_data.dart';
 import '../models/category.dart';
+import '../components/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({super.key});
@@ -20,7 +20,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
           itemCount: categoryMeals.length,
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(categoryMeals[index]);
           }),
     );
   }
