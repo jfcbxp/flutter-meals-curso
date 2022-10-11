d9931d8c67b62407b82c82c84b23c4415291e4c1
Joao Felipe <jfcbxp@gmail.com>
2022-10-11 08:35:42 -0300

    adicionado detalhes da receita

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:receitas/utils/app_routes.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
@@ -6,12 +7,14 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal, {super.key});
 
-  void _selectMeal() {}
+  void _selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(AppRoutes.MEAL_DETAIL, arguments: meal);
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: _selectMeal,
+      onTap: () => _selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         elevation: 4,
